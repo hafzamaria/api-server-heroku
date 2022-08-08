ce260be7c85e5516630a71a52b9a4ec479568818
mariadhantia <mariadhantia@gmail.com>
2022-08-08 21:12:00 +0500

    my changes

---
+++ api.mjs
@@ -0,0 +1,60 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+// let port = null;
+// if(process.env.PORT){
+//     port = process.env.PORT;
+// }else{
+//     port = 3000;
+// }
+
+let users =[];
+app.post("/user",(req,res)=>{
+
+    console.log(req.body);
+
+    users.push(req.body);
+
+    res.send("user is created");
+
+})
+
+app.get("/user",(req,res)=>{
+
+
+    res.send(users);
+
+})
+
+app.put("/user",(req,res)=>{
+
+
+    res.send("your user is modified");
+
+})
+
+app.delete("/user",(req,res)=>{
+
+
+    res.send("user is created");
+
+})
+
+app.get('/', (req, res) => {
+    console.log("ek request server per i");
+  res.send('Hello World!')
+})
+app.get('/profile', (req, res) => {
+    console.log("ek request server per i");
+  res.send('This is a profile!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
